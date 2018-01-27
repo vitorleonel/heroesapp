161222d50e92c63664ca0ede80da08370bf035c6
Vitor Leonel <falecom@vitorleonel.com>
2018-01-27 11:00:32 -0200

    adicionando bulma.io

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom'
 
 import '@styles/app.scss'
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('app'))
+ReactDOM.render(
+    <div>
+        <nav className="navbar has-shadow">
+            <div className="navbar-brand">
+                <a href="" className="navbar-item">
+                    <span className="title is-5">Hello, World!</span>
+                </a>
+            </div>
+        </nav>
+    </div>,
+    document.getElementById('app')
+)
