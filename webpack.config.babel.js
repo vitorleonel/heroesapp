7e83283288e847ed1c3175e8685174d2b07ff3d3
Vitor Leonel <falecom@vitorleonel.com>
2018-01-27 09:22:30 -0200

    hello world

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,33 @@
 import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
 
 export default {
 
-    entry: path.resolve(__dirname, 'src'),
+    entry: path.resolve(__dirname, 'src/js'),
 
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'app.bundle.js'
+        filename: 'js/app.bundle.js'
     },
 
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
 
-    resolve: {}
+    resolve: {},
+
+    plugins: [
+        new CleanWebpackPlugin(['public']),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        })
+    ]
 
 }
